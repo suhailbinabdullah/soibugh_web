dd0f516fcf88daef680fee7ee4f230b637e0d329
Suhail Bin Abdullah <suhail@mishantech.com>
2024-07-19 00:11:27 +0530

    seperated banner file'

--- soibugh_web/src/components/navbar/topbar/index.js
+++ soibugh_web/src/components/navbar/topbar/index.js
@@ -1,9 +1,5 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useNavigate } from 'react-router-dom';
 
 const TopBar = () => {
