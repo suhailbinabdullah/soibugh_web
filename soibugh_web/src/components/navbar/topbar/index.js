ca0d27d0a5fd038a8a511b266849d7df8d12195d
Suhail Bin Abdullah <suhail@mishantech.com>
2024-07-21 17:27:06 +0530

    removed all necessary toolbars

--- soibugh_web/src/components/navbar/topbar/index.js
+++ soibugh_web/src/components/navbar/topbar/index.js
@@ -1,14 +1,7 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useNavigate } from 'react-router-dom';
 
 const TopBar = () => {
-
-    const navigate = useNavigate()
     return (
         <Navbar expand="lg" bg="info" sticky='top'>
             <Container fluid>
