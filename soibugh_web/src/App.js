24dffc62eaaea4ac2e6f1ae9681cbda4bf83e321
Suhail Bin Abdullah <suhailbinabdullah@gmail.com>
2025-04-07 13:19:59 +0530

    removed the comment

--- soibugh_web/src/App.js
+++ soibugh_web/src/App.js
@@ -4,9 +4,6 @@ import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 import Navbar from './components/navbar';
 
-/* I am adding this comment just for git testing that from which user this is getting pushed, just changed the global emal and now testing again 
-now testing on random user*/
-
 function App() {
   return (
     <>
